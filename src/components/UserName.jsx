60e31c627e48c50dd5d3ae90c12bac4db74e2ed0
olgakahraman <ol.khrmn@gmail.com>
2024-05-27 21:57:05 -0400

    added google fonts and some styling to UserName component

--- src/components/UserName.jsx
+++ src/components/UserName.jsx
@@ -1,7 +1,11 @@
-import React from "react"
+import React from "react";
 
-export default function UserName(){
-    return (
-        <h1>UserName is working</h1>
-    )
+export default function UserName() {
+  return (
+    <div>
+      <h1>Olga Kahraman</h1>
+      <h3>Frontend Developer</h3>
+      <p>olga.kahraman.com</p>
+    </div>
+  );
 }
